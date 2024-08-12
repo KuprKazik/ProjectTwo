32d928541672363fc8d5c789e3e8b0f6a986871f
KuprKazik <kuprkazik94@gmail.com>
2024-08-12 12:21:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int totalCost = 9999;
+        int mileCost = 20;
+
+        int bonus = totalCost / mileCost;
+        System.out.println("Количество накопленных миль:" + bonus);
+    }
+}
